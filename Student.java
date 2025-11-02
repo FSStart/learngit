07e67e99572ac84485181171bfb247d82b9df624
Yasir <2060868697@qq.com>
2025-11-02 15:23:44 +0800

    第一次链接IDEA

--- Student.java
+++ Student.java
@@ -1,5 +1,8 @@
-public Class MyClass{
+import com.sun.security.jgss.GSSUtil;
+
+public class Student{
     public static void main(String[] arg){
-       
+        System.out.println("Hello gitHub");
+        System.out.println("命运就在当下，抓住当下");
     }
 }
